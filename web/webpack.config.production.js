8d737474e2393f188a65528676aa3bf23f58e7d1
Brady Holt <brady.holt@gmail.com>
2017-06-06 13:57:24 -0500

    Formatting

--- web/webpack.config.production.js
+++ web/webpack.config.production.js
@@ -14,6 +14,7 @@ config.externals = {
   'vue-router': 'VueRouter'
 };
 config.devtool = 'source-map';
+
 // Note: We are *replacing* the plugin config here.
 config.plugins = [
   new ExtractTextPlugin("styles.css"),
