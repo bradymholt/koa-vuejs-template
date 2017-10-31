11fc743cbacd7911e8db3384cdc9bce25e30254a
Brady Holt <brady.holt@gmail.com>
2017-10-31 08:59:30 -0500

    Do not auto run migrations when testing

--- api/db/migrations/1497840941079-idx_user_meta.ts
+++ api/db/migrations/1497840941079-idx_user_meta.ts
@@ -11,9 +11,9 @@ export class index1497840941079 implements MigrationInterface {
     connection: Connection,
     entityManager?: EntityManager
   ): Promise<any> {
-    queryRunner.query(
-      'CREATE INDEX index_user_meta ON "user" USING gin (meta jsonb_path_ops);'
-    );
+    queryRunner.query(`
+      CREATE INDEX index_user_meta ON "user" USING gin (meta jsonb_path_ops);
+      `);
   }
 
   public async down(
